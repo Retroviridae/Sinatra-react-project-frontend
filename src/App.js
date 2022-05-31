50a56fbee528655f2184fef72bf24e8c50b250e3
Alexander Schuessler <schuessler.alex@gmail.com>
2022-05-31 16:28:05 -0500

    Imported bootstrap navbar, made simple fetch requests display on the page.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar,Container,Nav,NavLink } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React,{ useState, useEffect } from 'react';
+
 
 function App() {
-  fetch("http://localhost:9292")
+  const [display,setDisplay] = useState([{name:'Home'},{name:"Blank"}])
+  useEffect(()=>{fetch("http://localhost:9292")
   .then((r) => r.json())
-  .then((data) => console.log(data));
+  .then((data) => setDisplay(data[2]))},[])
+  function destClick(){
+    fetch("http://localhost:9292/destinations")
+  .then((r) => r.json())
+  .then((data) => setDisplay(data));
+  }
+  function tripClick(){
+    fetch("http://localhost:9292/trips")
+    .then((r) => r.json())
+    .then((data) => setDisplay(data));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <Navbar bg="light" expand="lg">
+    <Container>
+      <Navbar.Brand href="/">
+      <img
+        alt=""
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Bike_Park_Bottom.JPG/250px-Bike_Park_Bottom.JPG"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      />{' '}
+    Tachyon Travel iTinerarys
+    </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto" >
+          <NavLink onClick={()=>setDisplay([{name:'Home'},{name:'Blank'}])} >Home</NavLink>
+          <NavLink onClick={destClick} to="/destrinations" >Destinations</NavLink>
+          <NavLink onClick={tripClick} to="/trips" >Trips</NavLink>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  {display.map(element=>{
+    return(
+      <>
+      <p>{element.name}</p>
+      <p>{element.title}</p>
+      <p>{element.arrival}</p>
+      </>
+    )
+  })}
+</div>
   );
 }
 
